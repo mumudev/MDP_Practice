2088926a7d68b318f19d763fdfd5db4a1c31e336
Tim Guo <tguo1@morningstar.com>
2016-08-24 10:45:31 +0800

    story2&&story3

--- js/index.js
+++ js/index.js
@@ -12,12 +12,17 @@
         _isReady = false;
 
     var $ = function(selector) {
+        if(selector instanceof $){
+            return selector;
+        }
         return new $.fn.init(selector);
     };
 
     $.VERSION = '1.0.0';
 
-    $.fn = $.prototype;
+    $.fn = $.prototype = {
+        constructor:$,
+    };
 
     $.fn.init = function(selector) {
         if (!selector)
@@ -58,6 +63,7 @@
                 idx.addEventListener(type, callback, useCapture);
             });
         }
+        return this;
     };
 
     $.fn.off = function(type, callback, useCapture) {
@@ -70,6 +76,7 @@
                 removeEventListener.call(idx, type, callback, useCapture);
             });
         }
+        return this;
     };
 
     $.each = function(elements, callback) {
@@ -119,6 +126,7 @@
         $.each(this, function(i, ele) {
             ele.style.display = 'block';
         });
+        return this;
     };
 
     $.fn.addClass = function(name) {
@@ -127,6 +135,7 @@
                 ele.className += name ? (' ' + name) : '';
             }
         });
+        return this;
     };
     $.fn.hasClass = function(name) {
         $.each(this, function(i, ele) {
@@ -139,6 +148,7 @@
             var classRE = new RegExp('(^|\\s)' + name + '(\\s|$)');
             ele.className = ele.className.replace(classRE, '');
         });
+        return this;
     };
     $.fn.toggleClass = function(name) {
         $.each(this, function(i, ele) {
@@ -148,19 +158,25 @@
                 $(ele).removeClass(name);
             }
         });
+        return this;
     };
 
     $.fn.css = function(attrName, attrValue) {
+        var ret = null;
         this.each(function() {
             if (typeof attrName === 'object') {
                 for (var key in attrName) {
                     key = camelize(key);
+                    
                     this.style[key] = attrName[key];
                 }
-            } else {
-                this.style[attrName] = attrValue;
+            } else if(attrValue){
+                this.style[camelize(attrName)] = attrValue;
+            }else{
+                ret =  window.getComputedStyle(this)[attrName];
             }
         });
+        return ret?ret:this;
 
     };
     var camelize = function(str) {
@@ -174,6 +190,7 @@
         $.each(this, function(i, ele) {
             ele.style.display = 'none';
         });
+        return this;
     };
     
 
@@ -181,27 +198,54 @@
         $.each(this, function(i, ele) {
             ele.style.display == 'none' ? $(ele).show() : $(ele).hide();
         });
+        return this;
     };
 
     $.fn.remove = function() {
         this.each(function() {
             //error --- ie 不支持remove
-            this.parentNode.removeChild(this);
+            this.parentNode&&this.parentNode.removeChild(this);
         });
+        return this;
     };
 
     $.fn.before = function(dom){
         this.each(function(index,ele){
             ele.parentNode.insertBefore(dom,ele);
         });
+        return this;
     };
 
-    $.fn.wap = function(dom){//$('#id').wap(nodeList)
+    $.fn.wrap = function(dom){//$('#id').rwap(nodeList)
         this.each(function(index, ele){
             $(ele).before(dom);
             dom.appendChild(ele);
         });
+        return this;
     };
+
+    $.fn.unwrap = function(selector){
+        this.each(function(index, ele){
+            var parent = $(ele).parent(selector);
+            parent&&parent.length&&parent[0].parentNode.replaceChild(ele,parent[0]);
+        });
+        return this;
+    };
+
+    $.fn.eq = function(i){
+        var len = this.length,
+            index = i + ( i<0 ? len : 0);
+        return $(this[index]);
+    };
+
+    $.fn.first = function(){
+        return this.eq(0);
+    };
+
+    $.fn.last = function(){
+        return this.eq(-1);
+    };
+
 
     $.fn.ready = function(fn) {
         if (readyList) {
@@ -230,6 +274,39 @@
         return '#' + ('00000' + (Math.random() * 0x1000000 << 0).toString(16)).slice(-6);
     };
 
+    $.fn.text = function(val){
+        return this[0].textContent = val;
+    };
+
+    $.fn.find = function(selector){
+         var elem = this[0].querySelectorAll(selector);
+         return $(elem);
+    };
+
+    $.fn.parent = function(selector){
+       selector = (selector.replace(/\.|#/g,''));
+       var parent = this[0].parentNode;
+       while(parent){
+            if(parent.className&&(parent.className.indexOf(selector)>-1 || selector === parent.id)){
+                return $(parent)
+           }
+           parent = parent.parentNode;
+       }
+    }
+
+    $.fn.next = function(){
+        var cur = this[0],
+            dir = 'nextSibling';
+        while ( ( cur = cur[ dir ] ) && cur.nodeType !== 1 ) {}
+        return $(cur);
+    };
+
+    $.fn.prev = function(){
+        var cur = this[0],
+            dir = 'prevSibling';
+        while ( ( cur = cur[ dir ] ) && cur.nodeType !== 1 ) {}
+        return $(cur);
+    };
 
     function Cache(){
         this.expando = 'JQ' + $.VERSION + Math.random().toString().replace(/\D/,'') + (Cache.uuid++);
@@ -274,10 +351,46 @@
             return  key===undefined ? this.cache(ele) : this.cache(ele)[key];  
         },
     };
+
+    var dataPriv = new Cache();
     var dataUser = new Cache();
 
-    $.fn.data = function(key, val) {
+    $.data = function(key, val) {
        return dataUser.access(this[0],key,val);
+    };
+
+    $.fn.data = function(key, val) {
+        var data = dataUser.access(this[0],key,val);
+        return data;
+        // if(typeof key === 'string' && val!== undefined){
+        //    return this[0].setAttribute('data-'+key,val);
+        // }else if(typeof key === 'object'){
+        //     for (var prop in key) {
+        //         this[0].setAttribute(prop,key[prop]);
+        //     }
+        // }else if(val === undefined){
+        //    return this[0].getAttribute('data-'+key);
+        // }
+    };
+
+    $.fn.delegate = function(eventType,selector,fn){
+        function handle(e){
+            var evt = window.event ?window.event:e;
+            var target = evt.target || evt.srcElement;
+            var currentTarget = e ? e.currentTarget:this;
+
+            selector = selector.replace(/\.|#/g,'');
+
+            while(currentTarget !== target || !currentTarget || !target){
+                if(target.id === selector || target.className.indexOf(selector)>-1 || target.nodeName.toLowerCase() === selector){
+                    evt.stopPropagation();
+                    return fn.call($(target),evt);
+                }
+                target  = target.parentNode;
+            }
+        }
+        this.on(eventType,handle);
+        return this;
     };    
 
     window.$ = $;
@@ -289,6 +402,38 @@
 //sizzle 选择器实现
 
 
+//继承实现
+function inherit(parent, child, flag) {
+    if (Object.prototype.toString.call(parent) !== '[object Function]') {
+        return console.log('parent not a valid class');
+    }
+    if (typeof child !== 'function') {
+        child = function() {};
+    }
+    if (flag) {
+        var supperClass = function() {};
+        supperClass.prototype = parent.prototype;
+        var oSuper = new supperClass();
+        for (var key in oSuper) { //区分hasOwnProperity
+            if (!child.prototype[key]) {
+                child.prototype[key] = oSuper[key];
+            }
+        }
+    } else {
+        var childClass = function() {};
+        childClass.prototype = child.prototype;
+        var oSub = new childClass();
+        child.prototype = new parent(); //复制整个父类
+        for (var key in oSub) { //恢复子类原型属性
+            if (!child.prototype[key]) {
+                child.prototype[key] = oSub[key];
+            }
+        }
+    }
+    child.__supper__ = parent.prototype;
+    child.prototype.constructor = child;
+    return child;
+}
 
 
 
---
+++ readme.md
@@ -0,0 +1,44 @@
+
+git 操作命令记录
+
+### git clone <远程仓库地址>
+example:git clone https://github.com/mumudev/MDP_Practice.git 
+
+
+
+
+### 本地文件操作
+添加到暂存区:git add . / git add *
+添加到版本库:git commit -m 'message' /git commit -a 包含add操作
+
+
+### 远程操作
+推送本地某个分支文件:
+git push <远程主机名> <本地分支名>:<远程分支名>
+推送所有分支:git push --all origin
+example: git push origin tim/story1
+配置远程主机名与地址映射关系：git remote add origin <远程仓库地址>
+拉取远程文件: git pull
+
+删除远程分支:git push origin :tim/story1 /  git push origin --delete tim/story1
+			 git branch -d -r branchname 
+
+
+
+### 本地操作
+初始化仓库：git init
+查看本地所有分支：git branch -a
+列出远程分支：git branch -r 
+分支创建并切换 git checkout -b <分支名>
+切换分支：git checkout <分支名>
+删除本地分支:git branch -d <分支名> 
+查看本地文件状态：git status
+合并分支：git merge <分支名>
+
+版本控制
+丢弃本地文件修改
+git checkout -- <filename>
+
+根据远程分支回退：
+git reset --hard origin/master
+git fetch origin
