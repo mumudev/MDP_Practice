964cd40a02c91637569a44bd12b3497a40b7d46d
ChasenWei <627777451@qq.com>
2016-08-31 19:08:54 +0800

    大概完成

--- chasen_story4/js/story4.js
+++ chasen_story4/js/story4.js
@@ -1,4 +1,3 @@
-var createList;
 
 var buttonData;
 var textData;
@@ -12,77 +11,10 @@ var divAct;
 var imageAct;
 var textAct;
 
-function loading() {
-    alert("loading");
-    var arr = document.getElementsByClassName("button");
-    for (var i = 0; i < arr.length; i++) {
-        arr[i].onmouseover = function() {
-            mouseOver(this);
-        };
-        arr[i].onmouseout = function() {
-            mouseOut(this);
-        };
-    }
-}
-
-function cancel() {
-    cancelMenu_type();
-    cancelMenu();
-}
-
-function test() {
-    alert("test");
-}
-
-function mouseOver(ele) {
-    ele.style.backgroundColor = "#3572b0";
-}
-
-function mouseOut(ele) {
-    ele.style.backgroundColor = "#dedede";
-}
-
-function getCurrentStyle(node) {
-    var style = null;
-    if (window.getComputedStyle) {
-        style = window.getComputedStyle(node, null);
-        //chrome、 firefox、 ie9
-        console.log('globalEventContext');
-    } else {
-        style = node.currentStyle;
-        //ie7、ie8
-        console.log('currentStyel');
-    }
-    return style;
-}
-
-
 /**
     global variable selected Element in contents
 */
 var selElement;
-
-/*
-    show operation menu
-*/
-function operate(ele, e) {
-    cancelMenu_type();
-    selElement = ele;
-    //cancel choosing other Elements in contents except selElement
-    unchooseOther(selElement);
-    var EVT = window.event ? window.event : e;
-    var menu = document.getElementsByClassName("menu")[0];
-    var x = EVT.clientX;
-    var y = EVT.clientY;
-    if (ele.style.border == "2px solid blue") {
-        cancelMenu();
-    } else {
-        ele.style.border = "2px solid blue";
-        menu.style.left = x + "px";
-        menu.style.top = y + "px";
-        menu.style.display = "block";
-    }
-}
 
 function showTypeMenu(event) {
     cancelMenu();
@@ -97,21 +29,12 @@ function showTypeMenu(event) {
         menuType.style.display = "block";
     }
 }
-/**
-    cancel choosing other Elements in contents except selElement
-*/
-function unchooseOther(ele) {
-    var childs = ele.parentNode.childNodes;
-    for (i = 0; i < childs.length; i++) {
-        if (childs[i] !== selElement) {
-            childs[i].style.border = "1px solid black";
-        }
-    }
-}
 
 function cancelMenu() {
-    var menu = document.getElementsByClassName("menu")[0];
-    menu.style.display = "none";
+    var menu = document.getElementsByClassName("menu");
+    for (var i = 0; i < menu.length; i++) {
+        menu[i].style.display = "none";
+    }
     if (selElement) {
         if (selElement.style.border) {
             selElement.style.border = "1px solid black";
@@ -119,46 +42,5 @@ function cancelMenu() {
     }
 }
 
-function cancelMenu_type() {
-    var menuType = document.getElementsByClassName("menu_type")[0];
-    menuType.style.display = "none";
-}
-
-
-/**
-    The operation's function
-**/
-
-function _delete() {
-    selElement.parentNode.removeChild(selElement);
-    cancelMenu();
-}
-
-function backcolor() {
-    selElement.style.backgroundColor = "rgb(" + ramdomMaker() +
-        "," + ramdomMaker() + "," + ramdomMaker() + ")";
-    cancelMenu();
-}
-
-function fontcolor() {
-    selElement.style.color = "rgb(" + ramdomMaker() +
-        "," + ramdomMaker() + "," + ramdomMaker() + ")";
-    cancelMenu();
-}
-
-function ramdomMaker() {
-    return Math.round(Math.random() * 255);
-}
-
-function fontSize(ele) {
-    var operate = ele.value;
-    if (operate == "+") {
-        selElement.style.fontSize = (parseInt(selElement.style.fontSize, 10) + 5) + "px";
-        cancelMenu();
-    } else {
-        selElement.style.fontSize = (parseInt(selElement.style.fontSize, 10) - 5) + "px";
-        cancelMenu();
-    }
-}
 
 /*document.onclick = function() { cancel(); };*/
--- chasen_story4/js/story4jQuery.js
+++ chasen_story4/js/story4jQuery.js
@@ -4,43 +4,51 @@ $(document).ready(function() {
         type: "get",
         url: "http://admadevwb8001:8001/api/html/elements",
         success: function(data) {
-            //alert(data);
-            createList = data;
             for (var i = 0; i < data.data.length; i++) {
                 $(".menu_type").append('<input class="head_button menu_button button menu_type_button" type="button" value="' + data.data[i].text + '" >');
             }
         }
     });
 
-    $(".menu_type").delegate(".menu_type_button","click", function create() {
+    $(".menu_type").delegate(".menu_type_button", "click", function create() {
         var content = document.getElementById("contents_p");
-        var eletype = this.value.toLowerCase();
+        var eletype = this.value.toLowerCase().replace(" ", "");
         var newNode = initializeNode(eletype);
         if (newNode) {
             content.appendChild(newNode);
         }
         cancelMenu_type();
     });
-//$.poxy();
+    //$.poxy();
 
 
     function initializeNode(eletype) {
         var newNode = null;
         if (eletype) {
-            if (eletype == "button" || eletype == "text") {
+            if (eletype == "inputtext") {
                 newNode = document.createElement("input");
-                newNode.type = eletype;
+                newNode.type = "text";
+                newNode.style.width = "110px";
+                newNode.style.height = "40px";
+            } else if (eletype == "image") {
+                newNode = document.createElement("img");
+            } else if (eletype == "button") {
+                newNode = document.createElement("button");
+                newNode.style.cursor = "pointer";
+                newNode.style.width = "110px";
                 newNode.style.height = "50px";
             } else {
                 newNode = document.createElement(eletype);
-                newNode.style.height = "100px";
+                newNode.style.width = "130px";
+                newNode.style.height = "130px";
             }
             newNode.className = eletype + "s";
-            newNode.onclick = function(e) {
-                operate(this, e);
-            };
+            /*bind event to the created elements */
+
+            //TODO ··············································································
+            bindEventOnCtdEle(newNode);
+            //TODO··············································································
             newNode.style.float = "left";
-            newNode.style.width = "100px";
             newNode.style.border = "1px solid black";
             newNode.style.fontSize = "15px";
             newNode = addNodeExampleContent(newNode);
@@ -48,32 +56,76 @@ $(document).ready(function() {
         return newNode;
     }
 
+    function bindEventOnCtdEle(newNode) {
+        $(newNode).bind("click", function operate() {
+            cancelMenu();
+            selElement = this;
+            //cancel choosing other Elements in contents except selElement
+            unchooseOther(selElement);
+            var EVT = window.event ? window.event : e;
+            var x = EVT.clientX;
+            var y = EVT.clientY;
+            var eletype = this.tagName.toLowerCase();
+            var menu = $("");
+            if (eletype == "button") {
+                menu = $(".menu_button_x");
+            } else if (eletype == "img") {
+                menu = $(".menu_image");
+            } else if (eletype == "div") {
+                menu = $(".menu_div");
+            } else if (eletype == "table") {
+                menu = $(".menu_table");
+            } else {
+                menu = $(".menu_text");
+            }
+            var border = this.style.border;
+            if (this.style.border == "2px solid blue") {
+                cancelMenu();
+            } else {
+                this.style.border = "2px solid blue";
+                menu.css("left", x + "px");
+                menu.css("top", y + "px");
+                menu.css("display", "block");
+            }
+        });
+    }
+
     function addNodeExampleContent(newNode) {
         var eletype = newNode.tagName.toLowerCase();
+        newNode = $(newNode);
         if (eletype == "table") {
-            var tr1 = document.createElement("tr");
-            var tr2 = document.createElement("tr");
-            var td1 = document.createElement("td");
-            td1.innerHTML = "A";
-            var td2 = document.createElement("td");
-            td2.innerHTML = "B";
-            var td3 = document.createElement("td");
-            td3.innerHTML = "C";
-            var td4 = document.createElement("td");
-            td4.innerHTML = "D";
-            tr1.appendChild(td1);
-            tr1.appendChild(td2);
-            tr2.appendChild(td3);
-            tr2.appendChild(td4);
-            newNode.appendChild(tr1);
-            newNode.appendChild(tr2);
+            //define a variable to loop through(traverse) the CellData Array
+            var cells_index = 0;
+            for (var i = 0; i < tableData.data.rows; i++) {
+                newNode.append("<tr></tr>");
+                for (var j = 0; j < tableData.data.cols; j++) {
+                    newNode.children("tr:eq(" + i + ")").append("<td></td>");
+                    //currentCell in returned CellData Array 
+                    var curCell = tableData.data.cells[cells_index];
+                    //if currentCell's row amd col will be show in the current item of Table 
+                    if (curCell.row == i && curCell.col == j) {
+                        //set the currentCell's data into the current item 
+                        newNode.children("tr:eq(" + i + ")").children("td:eq(" + j + ")").text(curCell.data);
+                        //jump into the next one in the returned CellData Array
+                        cells_index++;
+                    }
+                }
+            }
+        } else if (eletype == "input") {
+            newNode.val(textData.data.text);
         } else if (eletype == "div") {
-            newNode.innerHTML = eletype;
+            newNode.html(divData.data.text);
+        } else if (eletype == "button") {
+            newNode.html(buttonData.data.text);
+            newNode.css("cursor", "pointer");
+            newNode.attr("alt", buttonData.data.title);
         } else {
-            newNode.value = newNode.type;
+            newNode.attr("src", imageData.data.image);
+            newNode.attr("alt", imageData.data.title);
         }
-        return newNode;
+        return newNode[0];
     }
+
     // $.get("http://admadevwb8001:8001/api/html/elements", function(data) {
     //     alert(data);
     // });
@@ -127,47 +179,171 @@ $(document).ready(function() {
         }
     });
 
+    function appendItem(act, eleClass) {
+        var actArray = act.data;
+        for (var i = 0; i < actArray.length; i++) {
+            var newOne = $('<input class="head_button menu_button button" type="button" alt="' + actArray[i].title + '"value="' + actArray[i].text + '">');
+            $("." + eleClass).append(newOne);
+        }
+        if (actArray.length > 0) {
+            $("." + eleClass).css("height", "" + (5 + actArray.length * 55) + "px");
+        }
+    }
+
     $.ajax({
         type: "get",
         url: "http://admadevwb8001:8001/api/html/elements/button/actions",
         success: function(data) {
-            //alert(data);
-            imageData = data;
+            buttonAct = data;
+            appendItem(buttonAct, "menu_button_x");
         }
     });
+
     $.ajax({
         type: "get",
         url: "http://admadevwb8001:8001/api/html/elements/table/actions",
         success: function(data) {
-            //alert(data);
-            imageData = data;
+            tableAct = data;
+            appendItem(tableAct, "menu_table");
         }
     });
     $.ajax({
         type: "get",
         url: "http://admadevwb8001:8001/api/html/elements/div/actions",
         success: function(data) {
-            //alert(data);
-            imageData = data;
+            divAct = data;
+            appendItem(divAct, "menu_div");
         }
     });
     $.ajax({
         type: "get",
         url: "http://admadevwb8001:8001/api/html/elements/image/actions",
         success: function(data) {
-            //alert(data);
-            imageData = data;
+            imageAct = data;
+            appendItem(imageAct, "menu_image");
         }
     });
     $.ajax({
         type: "get",
         url: "http://admadevwb8001:8001/api/html/elements/inputText/actions",
         success: function(data) {
-            //alert(data);
-            imageData = data;
+            textAct = data;
+            appendItem(textAct, "menu_text");
         }
     });
-    $("head_button menu_button button")
+
+    function loading() {
+        alert("loading");
+        var arr = document.getElementsByClassName("button");
+        for (var i = 0; i < arr.length; i++) {
+            arr[i].onmouseover = function() {
+                mouseOver(this);
+            };
+            arr[i].onmouseout = function() {
+                mouseOut(this);
+            };
+        }
+    }
+
+    function cancel() {
+        cancelMenu_type();
+        cancelMenu();
+    }
+
+    function test() {
+        alert("test");
+    }
+
+    function mouseOver(ele) {
+        ele.style.backgroundColor = "#3572b0";
+    }
+
+    function mouseOut(ele) {
+        ele.style.backgroundColor = "#dedede";
+    }
+
+    function getCurrentStyle(node) {
+        var style = null;
+        if (window.getComputedStyle) {
+            style = window.getComputedStyle(node, null);
+            //chrome、 firefox、 ie9
+            console.log('globalEventContext');
+        } else {
+            style = node.currentStyle;
+            //ie7、ie8
+            console.log('currentStyel');
+        }
+        return style;
+    }
+
+
+
+    /**
+        cancel choosing other Elements in contents except selElement
+    */
+    function unchooseOther(ele) {
+        cancelMenu();
+        var childs = $(ele).parent().children();
+        for (i = 0; i < childs.length; i++) {
+            if (childs[i] !== selElement) {
+                childs[i].style.border = "1px solid black";
+            }
+        }
+    }
+
+    function cancelMenu_type() {
+        var menuType = document.getElementsByClassName("menu_type")[0];
+        menuType.style.display = "none";
+    }
+
+
+    /**
+        The operation's function
+    **/
+    var operateFunction = {
+        _delete: function() {
+            selElement.parentNode.removeChild(selElement);
+            cancelMenu();
+        },
+        backcolor: function() {
+            selElement.style.backgroundColor = "rgb(" + ramdomMaker() +
+                "," + ramdomMaker() + "," + ramdomMaker() + ")";
+            cancelMenu();
+        },
+        fontcolor: function() {
+            selElement.style.color = "rgb(" + ramdomMaker() +
+                "," + ramdomMaker() + "," + ramdomMaker() + ")";
+            cancelMenu();
+        },
+        _fontSize: function() {
+            var operate = this.value.charAt(this.value.length - 1);
+            if (operate == "+") {
+                selElement.style.fontSize = (parseInt(selElement.style.fontSize, 10) + 2) + "px";
+                cancelMenu();
+            } else {
+                selElement.style.fontSize = (parseInt(selElement.style.fontSize, 10) - 2) + "px";
+                cancelMenu();
+            }
+        }
+    };
+
+    function ramdomMaker() {
+        return Math.round(Math.random() * 255);
+    }
+
+    function bindEventToOpreItem(eleClass) {
+        $("." + eleClass).delegate("input:first", "click", operateFunction._delete);
+        $("." + eleClass).delegate("input:eq(1)", "click", operateFunction.backcolor);
+        $("." + eleClass).delegate("input:eq(2)", "click", operateFunction.fontcolor);
+        $("." + eleClass).delegate("input:gt(2)", "click", operateFunction._fontSize);
+    }
+    bindEventToOpreItem("menu_table");
+    bindEventToOpreItem("menu_image");
+    bindEventToOpreItem("menu_div");
+    bindEventToOpreItem("menu_button_x");
+    bindEventToOpreItem("menu_text");
+
+
 });
 
 
