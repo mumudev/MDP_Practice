e9c59d52ca0540fd89d88c3ccd45ac9250ca464f
ChasenWei <627777451@qq.com>
2016-10-20 13:59:33 +0800

    update

---
+++ chasen_story7/js/story7jQuery.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+    //------------------------------------------------------------------------------------------------
+    // var urldata = new dataUrlModel();
+    // var samplemodel = new menuSampleModel(urldata.get("sampleData" + eletype));
+    var v1 = new menuTypeView();
+    // new elements({ el: $("#contents_p") });
+    var v2 = new elements();
+});
